82cde77b9250bcd27b237909f03a078d13c31cd2
hrupesh <rcashok0@gmail.com>
2020-09-01 22:36:11 +0530

    Auto Commit 😁

--- wrongsub.py
+++ wrongsub.py
@@ -1,12 +1,17 @@
-n = input("Enter Number: ")
-k = input("Enter no of times number: ")
-i = 0
-for i in range(k) :
-    if(n % 10 == 0):
-        n = n/10
-    else:
-          # print("In else")
-          n = n - 1
-    # print(n)
+# n = input("Enter Number: ")
+# k = input("Enter no of times number: ")
+# i = 0
+# for i in range(k):
+#     if(n % 10 == 0):
+#         n = n/10
+#     else:
+#         # print("In else")
+#         n = n - 1
+#     # print(n)
 
+# print(n)
+
+n, k = input().split()
+k = int(k)
+n = n[::-1]
 print(n)
