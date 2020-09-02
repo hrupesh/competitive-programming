cb6f4cca36bd3fa8a2a0997fee3055cb27d8279a
hrupesh <rcashok0@gmail.com>
2020-09-02 13:42:32 +0530

    Auto Commit 😁

--- waytolongwords.py
+++ waytolongwords.py
@@ -1,19 +1,29 @@
-# n = input()
-# n = int(n)
-# words = []
+n = input()
+n = int(n)
+words = []
 
-# for i in range(n):
-#     t = input()
-#     words.append(t)
-#     # print(words[i])
 
-# for i in words:
-#     if(len(i) > 10):
-#         print(i)
-#     # print("\n")
-# # print(words)
+def converttoabbr(a):
+    print(a[0])
+    b = a[::-1]
+    print(b[0])
+    new = a[0] + str(len(a) - 2) + b[0]
+    return new
 
-a = "rupesh"
-print(a[0])
-b = a[::-1]
-print(b[0])
+
+for i in range(n):
+    t = input()
+    words.append(t)
+    # print(words[i])
+
+
+for i in words:
+    if(len(i) > 10):
+        i = converttoabbr(i)
+        print(i)
+    # print("\n")
+print(words)
+
+
+# c = input()
+# convertoabbr(c)
