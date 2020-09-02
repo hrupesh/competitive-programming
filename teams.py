46cb8cdfb433c6cb3d9a9687e33221d846bb7389
hrupesh <rcashok0@gmail.com>
2020-09-02 15:42:57 +0530

    Auto Commit 😁

--- teams.py
+++ teams.py
@@ -1,6 +1,13 @@
 n = input()
+n = int(n)
 solved = 0
+qs = []
 
 for i in range(n):
     q = input()
-    print(q)
+    qs.append(q)
+    if("1 1" in q):
+        solved = solved + 1
+    # print(q)
+
+print(solved)
