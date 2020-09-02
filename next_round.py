5490366d24547fdb29787599e03e31c8549a6483
hrupesh <rcashok0@gmail.com>
2020-09-02 19:18:54 +0530

    Auto Commit 😁

--- next_round.py
+++ next_round.py
@@ -1,6 +1,17 @@
 n, k = input().split()
 # contestants = []
 n = int(n)
+k = int(k)
 contestants = (input().split())
+advanced = 0
+l = int(contestants[k])
+# print(l)
 
-print(contestants)
+for i in contestants:
+    i = int(i)
+    if(i >= l):
+        advanced = advanced + 1
+
+print(advanced)
+
+# print(contestants)
