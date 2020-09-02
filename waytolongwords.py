aa9cc96463578d64e6439256c8efe8a328fc317b
hrupesh <rcashok0@gmail.com>
2020-09-02 12:11:30 +0530

    Auto Commit 😁

--- waytolongwords.py
+++ waytolongwords.py
@@ -1,14 +1,19 @@
-n = input()
-n = int(n)
-words = []
+# n = input()
+# n = int(n)
+# words = []
 
-for i in range(n):
-    t = input()
-    words.append(t)
-    # print(words[i])
+# for i in range(n):
+#     t = input()
+#     words.append(t)
+#     # print(words[i])
 
-for i in words:
-    if(len(i) > 10):
-        print(i)
-    # print("\n")
-# print(words)
+# for i in words:
+#     if(len(i) > 10):
+#         print(i)
+#     # print("\n")
+# # print(words)
+
+a = "rupesh"
+print(a[0])
+b = a[::-1]
+print(b[0])
