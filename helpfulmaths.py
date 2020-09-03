5ccfdb8a7b121a663946b1de383d95b8e40cc29c
hrupesh <rcashok0@gmail.com>
2020-09-04 01:09:27 +0530

    Auto Commit 😁

--- helpfulmaths.py
+++ helpfulmaths.py
@@ -1,4 +1,8 @@
 s = input()
 # s = int(s, base=36)
-# for i in range(s.count)
-print(len(s))
+sorted = []
+for i in range(len(s)):
+    if(i % 2 == 0):
+        sorted.append(s[i])
+        print(s[i])
+# print(len(s))
