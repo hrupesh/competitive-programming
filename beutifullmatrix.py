4e9419c9aca179043351afd46814beadee1300c0
hrupesh <rcashok0@gmail.com>
2020-09-03 20:28:33 +0530

    Auto Commit 😁

--- beutifullmatrix.py
+++ beutifullmatrix.py
@@ -4,10 +4,14 @@ for i in range(5):
     matrix.append(m)
 
 index_y= 1
+index_x= 0
 
 for i in matrix:
     if("1" in i):
         print(index_y)
+        # print(i.find("1"))
+        index_x = i.find("1")
+        print(index_x)
     index_y = index_y + 1
 
-print(matrix)
+# print(matrix)
