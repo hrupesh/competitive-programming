770707b48cb9c054eb9f0207942d484247e4e080
hrupesh <rcashok0@gmail.com>
2020-09-04 01:12:15 +0530

    Auto Commit 😁

--- helpfulmaths.py
+++ helpfulmaths.py
@@ -1,8 +1,10 @@
 s = input()
 # s = int(s, base=36)
-sorted = []
+sorted_s = []
 for i in range(len(s)):
     if(i % 2 == 0):
-        sorted.append(s[i])
+        sorted_s.append(int(s[i]))
         print(s[i])
-# print(len(s))
+
+sorted_s = sorted(sorted_s)
+print(sorted_s)
