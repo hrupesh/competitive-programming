062322ed1db880f1ab33fe78ee98caae0a4cf91c
hrupesh <rcashok0@gmail.com>
2020-09-03 20:14:52 +0530

    Auto Commit 😁

--- beutifullmatrix.py
+++ beutifullmatrix.py
@@ -2,5 +2,12 @@ matrix = []
 for i in range(5):
     m = input()
     matrix.append(m)
-    
+
+index_y= 1
+
+for i in matrix:
+    if("1" in i):
+        print(index_y)
+    index_y = index_y + 1
+
 print(matrix)
