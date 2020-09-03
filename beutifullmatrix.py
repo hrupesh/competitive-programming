46c2b9cc6c1d8528595968d671263bfcb967a8f6
hrupesh <rcashok0@gmail.com>
2020-09-03 20:41:48 +0530

    Auto Commit 😁

--- beutifullmatrix.py
+++ beutifullmatrix.py
@@ -3,36 +3,44 @@ for i in range(5):
     m = input()
     matrix.append(m)
 
-index_y = 1
-index_x = 0
+pos_i = 1
+pos_y = 0
 
 for i in matrix:
     if("1" in i):
-        print(index_y)
+        print(pos_i)
         # print(i.find("1"))
-        index_x = i.find("1")
-        print(index_x)
-    index_y = index_y + 1
+        pos_y = i.find("1")
+        print(pos_y)
+    pos_i = pos_i + 1
 
 moves = 0
 
-if(index_x == 1 or index_x == 5):
+if(pos_i == 1 or pos_i == 5):
     moves = 2
-elif(index_x == 2 or index_x == 4):
+    print("In 1 5  for i")
+elif(pos_i == 2 or pos_i == 4):
     moves = 1
-elif(index_x == 3):
-    moves = 0
+    print("In 2 4 for i")
+elif(pos_i == 3):
+    pass
+    print("In 3 for i")
 else:
-    moves = 0
+    pass
+    print("In else for i")
 
-if(index_y == 0 or index_y == 8):
+if(pos_y == 0 or pos_y == 8):
     moves = moves + 2
-elif(index_y == 2 or index_y == 6):
+    print("In 0 8 for y")
+elif(pos_y == 2 or pos_y == 6):
     moves = moves + 1
-elif(index_y == 4):
+    print("In 2 6 for y")
+elif(pos_y == 4):
     pass
+    print("In 4 for y")
 else:
     pass
+    print("In else for y")
 
 print(moves)
 
