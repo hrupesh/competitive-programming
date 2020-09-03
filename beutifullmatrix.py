bc5d7fe413d6248bae99904a3daac3fd226ec5ba
hrupesh <rcashok0@gmail.com>
2020-09-03 20:54:43 +0530

    Auto Commit 😁

--- beutifullmatrix.py
+++ beutifullmatrix.py
@@ -16,28 +16,37 @@ for i in matrix:
 
 moves = 0
 
-if(pos_i == 1 or pos_i == 5):
-    moves = moves + 2
-    print("In 1 5  for i")
-elif(pos_i == 2 or pos_i == 4):
-    moves = moves + 1
-    print("In 2 4 for i")
-else:
-    moves = 0
+
+def calc_i():
+    if(pos_i == 1 or pos_i == 5):
+        return 2
+        print("In 1 5  for i")
+    elif(pos_i == 2 or pos_i == 4):
+        return 1
+        print("In 2 4 for i")
+    else:
+        return 0
 
 
-if(pos_y == 0 or pos_y == 8):
-    moves = moves + 2
-    print("In 0 8 for y")
-elif(pos_y == 2 or pos_y == 6):
-    moves = moves + 1
-    print("In 2 6 for y")
-elif(pos_y == 4):
-    pass
-    print("In 4 for y")
-else:
-    pass
-    print("In else for y")
+def calc_y():
+    if(pos_y == 0 or pos_y == 8):
+        return 2
+        print("In 0 8 for y")
+    elif(pos_y == 2 or pos_y == 6):
+        return 1
+        print("In 2 6 for y")
+    elif(pos_y == 4):
+        return 0
+        print("In 4 for y")
+    else:
+        pass
+        print("In else for y")
+
+
+moves_i = calc_i()
+moves_j = calc_y()
+
+moves = moves_i + moves_j
 
 print(moves)
 
