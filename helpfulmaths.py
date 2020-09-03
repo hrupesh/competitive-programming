2e1b1d087278bebf831960c05cad746194f66968
hrupesh <rcashok0@gmail.com>
2020-09-04 01:17:56 +0530

    Auto Commit 😁

--- helpfulmaths.py
+++ helpfulmaths.py
@@ -4,12 +4,15 @@ sorted_s = []
 for i in range(len(s)):
     if(i % 2 == 0):
         sorted_s.append(int(s[i]))
-        print(s[i])
+        # print(s[i])
 
 sorted_s = sorted(sorted_s)
 
 new_s = ""
-for i in sorted_s:
-    new_s = new_s + i + "+"
+for i in range(len(sorted_s)):
+    if(i == len(sorted_s)-1):
+        new_s = new_s + str(sorted_s[i])
+    else:
+        new_s = new_s + str(sorted_s[i]) + "+"
 
 print(new_s)
