c271ff9f9df3da42dcaccc6054b4b4bd702b20e7
hrupesh <rcashok0@gmail.com>
2020-09-03 19:05:17 +0530

    Auto Commit 😁

--- lexographicstrings.py
+++ lexographicstrings.py
@@ -1,3 +1,14 @@
 str1 = input()
+str2 = input()
 str1 = int(str1, base=36)
-print(str1)
+str2 = int(str2, base=36)
+
+if(str1 > str2):
+    print(1)
+elif(str1 == str2):
+    print(0)
+else:
+    print(-1)
+
+# print(str1)
+# print(str2)
