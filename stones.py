74fa65c47b33b13bcf50c9972625a8094bd6358e
hrupesh <rcashok0@gmail.com>
2020-09-05 00:00:40 +0530

    Auto Commit 😁

--- stones.py
+++ stones.py
@@ -10,9 +10,12 @@ for i in range(len(stones)):
     # print(i, stones[i])
     if(i < len(stones) - 1 and i > 0):
         if(stones[i] != stones[i+1] and stones[i] != stones[i-1]):
-            print(0)
+            # print(0)
             break
-        elif(stones[i] == stones[i+1] or stones[i] == stones[i-1]):
+
+    if(i < len(stones) - 1):
+        if(stones[i] == stones[i+1]):
+            # print(stones[i], steps)
             steps = steps + 1
 
 print(steps)
