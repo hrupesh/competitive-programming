c951acbba08e8f190caf55a591fff57e076a63e4
hrupesh <rcashok0@gmail.com>
2020-09-04 14:09:49 +0530

    Auto Commit 😁

--- ringroad.py
+++ ringroad.py
@@ -17,18 +17,18 @@ print(new_a)
 def move(moves):
     for i in range(n):
         print(i+1)
+        moves = moves + 1
         if(i+1 == new_a[0]):
             new_a.pop(0)
             print("Popping")
         if(len(new_a) == 0):
             return moves
-        moves = moves + 1
 
-    move(moves)
+    # move(moves)
 
 
 moves = move(0)
-print("Moves made : "+str(moves))
+print("Moves made : "+str(moves-1))
 
 print(new_a)
 
