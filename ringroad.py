5107a86f2ceb8872d59c0194d9c38c2792513ed1
hrupesh <rcashok0@gmail.com>
2020-09-04 12:32:31 +0530

    Auto Commit 😁

--- ringroad.py
+++ ringroad.py
@@ -4,5 +4,28 @@ m = int(m)
 
 a = input()
 
+new_a = []
+
+for i in range(len(a)):
+    # print(a[i],i)
+    a[i] = int(a[i])
+    if(a[i] % 2 == 0):
+        new_a.append(a[i])
+
+print(new_a)
+
+
+def move(moves):
+    for i in range(n):
+        print(i)
+        moves = moves + 1
+
+    if(moves == n):
+        return moves
+    move(moves)
+
+
+moves = move(0)
+print("Moves made : "+str(moves))
 
 print(a)
