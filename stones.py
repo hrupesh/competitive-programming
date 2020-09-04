4bbd72dc1a92363e6a8908bb94a4b2fe15657455
hrupesh <rcashok0@gmail.com>
2020-09-04 23:51:28 +0530

    Auto Commit 😁

--- stones.py
+++ stones.py
@@ -4,7 +4,15 @@ n = int(n)
 
 stones = input()
 
+steps = 0
+
 for i in range(len(stones)):
-    print(i, stones[i])
-    if(stones[i] != stones[i+1]):
-        print(0)
+    # print(i, stones[i])
+    if(i < len(stones) - 1 and i > 0):
+        if(stones[i] != stones[i+1] and stones[i] != stones[i-1]):
+            print(0)
+            break
+        elif(stones[i] == stones[i+1] or stones[i] == stones[i-1]):
+            steps = steps + 1
+
+print(steps)
