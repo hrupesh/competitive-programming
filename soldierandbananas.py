16a1f411badf4e3e6f36d4d1fe0d562eeac7423e
hrupesh <rcashok0@gmail.com>
2020-09-06 00:06:18 +0530

    Auto Commit 😁

--- soldierandbananas.py
+++ soldierandbananas.py
@@ -8,9 +8,11 @@ total = 0
 for i in range(w):
     total = total + k*(i+1)
 
-# print(total)
+print(total)
 
-if(total == 1 or n > 100000000 or total < 0):
+needed = total - n
+
+if(total == 1 or needed < 0):
     print(0)
 else:
-    print(total - n)
+    print(needed)
