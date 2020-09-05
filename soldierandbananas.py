b13d0d3ba96c1c080e08a4a3d59eb411477541ea
hrupesh <rcashok0@gmail.com>
2020-09-05 17:51:50 +0530

    Auto Commit 😁

--- soldierandbananas.py
+++ soldierandbananas.py
@@ -1,9 +1,13 @@
-k, n, w = int(input()).split()
+k, n, w = input().split()
+k = int(k)
+n = int(n)
+w = int(w)
 
 total = 0
 
 for i in range(w):
-    total = total + k*n+1
+    total =  k*i+1
+    print(total,i)
 
 print(total)
 
