ea90504e5dae034093756594d9b4df68b86747db
hrupesh <rcashok0@gmail.com>
2020-09-06 03:02:26 +0530

    Auto Commit 😁

--- bearbrothers.py
+++ bearbrothers.py
@@ -1,3 +1,15 @@
-a, b = int(input()).split()
+a, b = input().split()
+a = int(a)
+b = int(b)
 
+years = 0
 
+for(i in range(b)):
+    a = a*3
+    b = b*2
+    if(a > b){
+        years = i
+        break
+    }
+    
+print(years)
