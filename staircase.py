239676c216831a20eabc07069202a71dbb307f12
hrupesh <rcashok0@gmail.com>
2020-09-07 04:05:23 +0530

    Auto Commit 😁

--- staircase.py
+++ staircase.py
@@ -11,6 +11,8 @@ import sys
 
 def staircase(n):
     for i in range(n):
+        for k in range(i):
+              print("$", end="")
         for j in range(i+1):
             print("#", end="")
         print(" ")
