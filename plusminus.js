c6615bf55a5fc9b7362cdf233f2ccc816213d2f6
hrupesh <rcashok0@gmail.com>
2020-09-06 21:04:14 +0530

    Auto Commit 😁

--- plusminus.js
+++ plusminus.js
@@ -1,61 +1,62 @@
-"use strict";
+'use strict';
 
 process.stdin.resume();
-process.stdin.setEncoding("utf-8");
+process.stdin.setEncoding('utf-8');
 
-let inputString = "";
+let inputString = '';
 let currentLine = 0;
 
-process.stdin.on("data", (inputStdin) => {
-  inputString += inputStdin;
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
 });
 
-process.stdin.on("end", (_) => {
-  inputString = inputString
-    .replace(/\s*$/, "")
-    .split("\n")
-    .map((str) => str.replace(/\s*$/, ""));
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
 
-  main();
+    main();
 });
 
 function readLine() {
-  return inputString[currentLine++];
+    return inputString[currentLine++];
 }
 
 // Complete the plusMinus function below.
 function plusMinus(arr) {
-  var positive = 0;
-  var negative = 0;
-  var zeroes = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      positive += 1;
-    } else if (arr[i] < 0) {
-      negative += 1;
-    } else {
-      zeroes += 1;
+    var positive = 0.000000
+    var negative = 0.000000
+    var zeroes = 0.000000
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            positive += 1
+        }
+        else if (arr[i] < 0) {
+            negative += 1
+        } else {
+            zeroes += 1
+        }
     }
-  }
-  positive = positive / arr.length;
-  positive.toFixed(6);
-  negative = negative / arr.length;
-  negative.toFixed(6);
-  zeroes = zeroes / arr.length;
-  zeroes.toFixed(6);
 
-  console.log(positive);
-  console.log(negative);
-  console.log(zeroes);
+    positive = positive / arr.length
+    positive = positive.toFixed(6)
+    negative = negative / arr.length
+    negative = negative.toFixed(6)
+    zeroes = zeroes / arr.length
+    zeroes = zeroes.toFixed(6)
+
+    console.log(positive)
+    console.log(negative)
+    console.log(zeroes)
+
 }
 
 function main() {
-  const n = parseInt(readLine(), 10);
+    const n = parseInt(readLine(), 10);
 
-  const arr = readLine()
-    .split(" ")
-    .map((arrTemp) => parseInt(arrTemp, 10));
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
 
-  plusMinus(arr);
+    plusMinus(arr);
 }
