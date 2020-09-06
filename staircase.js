d21731e54a0fdc7efdf394eed729828a88c43bd5
hrupesh <rcashok0@gmail.com>
2020-09-07 03:24:24 +0530

    Auto Commit 😁

--- staircase.js
+++ staircase.js
@@ -1,36 +1,35 @@
-'use strict';
+"use strict";
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 // Complete the staircase function below.
 function staircase(n) {
-
-    console.log(n)
-
+  console.log(n);
 }
 
 function main() {
-    const n = parseInt(readLine(), 10);
+  const n = parseInt(readLine(), 10);
 
-    staircase(n);
+  staircase(n);
 }
