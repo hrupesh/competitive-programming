61664770f1ac3131f3845f8f82db3e10db6ffb2f
hrupesh <rcashok0@gmail.com>
2020-09-07 13:20:01 +0530

    Auto Commit 😁

--- timeconversion.js
+++ timeconversion.js
@@ -1,52 +1,62 @@
-"use strict";
+'use strict';
 
-const fs = require("fs");
+const fs = require('fs');
 
 process.stdin.resume();
-process.stdin.setEncoding("utf-8");
+process.stdin.setEncoding('utf-8');
 
-let inputString = "";
+let inputString = '';
 let currentLine = 0;
 
-process.stdin.on("data", (inputStdin) => {
-  inputString += inputStdin;
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
 });
 
-process.stdin.on("end", (_) => {
-  inputString = inputString
-    .trim()
-    .split("\n")
-    .map((str) => str.trim());
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
 
-  main();
+    main();
 });
 
 function readLine() {
-  return inputString[currentLine++];
+    return inputString[currentLine++];
 }
 
 /*
  * Complete the timeConversion function below.
  */
 function timeConversion(s) {
-  var hrs = s.slice(0, 2);
-  hrs = parseInt(hrs);
 
-  if (s.includes("PM")) {
-    return "Time in PM" + hrs;
-  } else if (s.includes("AM")) {
-    return "Time in AM";
-  }
+    var hrs = s.slice(0, 2)
+    var rest = s.slice(2, 8)
+    hrs = parseInt(hrs)
+
+    if (s.includes("PM")) {
+        if (hrs == 12) {
+            hrs = "00"
+        } else {
+            hrs += 12
+        }
+        return  hrs + rest
+    } else if (s.includes("AM")) {
+        if (hrs == 12) {
+            hrs = "00"
+        }else if(hrs < 10){
+            hrs = "0"+hrs
+        }
+        return hrs + rest
+    }
+
 }
 
 function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-  const s = readLine();
+    const s = readLine();
 
-  let result = timeConversion(s);
+    let result = timeConversion(s);
 
-  ws.write(result + "\n");
+    ws.write(result + "\n");
 
-  ws.end();
+    ws.end();
 }
