23917abeb89a8db4daea77d3248a9d53b6ce18f3
hrupesh <rcashok0@gmail.com>
2020-09-07 17:02:57 +0530

    Auto Commit 😁

--- grading.js
+++ grading.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function (inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function () {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -30,24 +30,24 @@ function readLine() {
  */
 
 function gradingStudents(grades) {
-    return grades
+  return grades;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const gradesCount = parseInt(readLine().trim(), 10);
+  const gradesCount = parseInt(readLine().trim(), 10);
 
-    let grades = [];
+  let grades = [];
 
-    for (let i = 0; i < gradesCount; i++) {
-        const gradesItem = parseInt(readLine().trim(), 10);
-        grades.push(gradesItem);
-    }
+  for (let i = 0; i < gradesCount; i++) {
+    const gradesItem = parseInt(readLine().trim(), 10);
+    grades.push(gradesItem);
+  }
 
-    const result = gradingStudents(grades);
+  const result = gradingStudents(grades);
 
-    ws.write(result.join('\n') + '\n');
+  ws.write(result.join("\n") + "\n");
 
-    ws.end();
+  ws.end();
 }
