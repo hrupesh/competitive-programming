33da6267c3e26f4fb1b708676f4192ca15c17230
hrupesh <rcashok0@gmail.com>
2020-09-09 21:03:09 +0530

    Auto Commit 😁

--- betweentwosets.js
+++ betweentwosets.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function (inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function () {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -32,49 +32,55 @@ function readLine() {
  */
 
 function getTotalX(a, b) {
-    var X = []
-    var n = a[a.length - 1]
-    var i = 2
-    var t = n
-    while (t <= b[0]) {
-        X.push(t)
-        t = n * i
-        i++
+  var X = [];
+  var n = a[a.length - 1];
+  var i = 2;
+  var t = n;
+  while (t <= b[0]) {
+    X.push(t);
+    t = n * i;
+    i++;
+  }
+
+  var newX = [];
+
+  X.forEach((i) => {
+    var okay = true;
+    b.forEach((j) => {
+      if (!(j % i == 0)) {
+        okay = false;
+      }
+    });
+    if (okay) {
+      newX.push(i);
     }
+  });
 
-    var newX = []
-
-    X.forEach((i) => {
-        var okay = true
-        b.forEach((j) => {
-            if (!(j % i == 0)) {
-                okay = false
-            }
-        })
-        if (okay) {
-            newX.push(i)
-        }
-    })
-
-    return newX.length
+  return newX.length;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+  const firstMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
 
-    const n = parseInt(firstMultipleInput[0], 10);
+  const n = parseInt(firstMultipleInput[0], 10);
 
-    const m = parseInt(firstMultipleInput[1], 10);
+  const m = parseInt(firstMultipleInput[1], 10);
 
-    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
 
-    const brr = readLine().replace(/\s+$/g, '').split(' ').map(brrTemp => parseInt(brrTemp, 10));
+  const brr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((brrTemp) => parseInt(brrTemp, 10));
 
-    const total = getTotalX(arr, brr);
+  const total = getTotalX(arr, brr);
 
-    ws.write(total + '\n');
+  ws.write(total + "\n");
 
-    ws.end();
+  ws.end();
 }
