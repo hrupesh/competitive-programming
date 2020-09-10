0df2fc163611cb1857bb8d50c3d06931157bde1f
hrupesh <rcashok0@gmail.com>
2020-09-11 02:42:43 +0530

    Auto Commit 😁

--- dynamicarray.py
+++ dynamicarray.py
@@ -4,27 +4,23 @@ n, q = map(int, raw_input().split(" "))
 
 lastans = 0
 
-
 def one(x, y, lastans):
-    temp = (x ^ lastans) % n
-    if temp not in seq:
-        seq[temp] = [y]
-    else:
-        seq[temp] = seq[temp] + [y]
-    return lastans
-
+	temp = (x ^ lastans) % n
+	if temp not in seq:
+		seq[temp] = [y]
+	else:
+		seq[temp] = seq[temp] + [y]
+	return lastans
 
 def two(x, y, lastans):
-    temp = (x ^ lastans) % n
-    pos = y % len(seq[temp])
-    lastans = seq[temp][pos]
-    print lastans
-    return lastans
+	temp = (x ^ lastans) % n
+	pos = y % len(seq[temp])
+	lastans = seq[temp][pos]
+	print lastans
+	return lastans
 
 
 for x in range(q):
-    val, x, y = map(int, raw_input().split(" "))
-    if (val == 1):
-        lastans = one(x, y, lastans)
-    else:
-        lastans = two(x, y, lastans)
+	val, x, y = map(int, raw_input().split(" "))
+	if (val == 1): lastans = one(x, y, lastans)
+	else: lastans = two(x, y, lastans)
