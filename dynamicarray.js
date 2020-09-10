a36de057e3458f79ceed73c9ce8499899698d0e5
hrupesh <rcashok0@gmail.com>
2020-09-10 20:18:48 +0530

    Auto Commit 😁

--- dynamicarray.js
+++ dynamicarray.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -32,28 +32,30 @@ function readLine() {
  */
 
 function dynamicArray(n, queries) {
-    // Write your code here
-
+  // Write your code here
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+  const firstMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
 
-    const n = parseInt(firstMultipleInput[0], 10);
+  const n = parseInt(firstMultipleInput[0], 10);
 
-    const q = parseInt(firstMultipleInput[1], 10);
+  const q = parseInt(firstMultipleInput[1], 10);
 
-    let queries = Array(q);
+  let queries = Array(q);
 
-    for (let i = 0; i < q; i++) {
-        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
-    }
+  for (let i = 0; i < q; i++) {
+    queries[i] = readLine()
+      .replace(/\s+$/g, "")
+      .split(" ")
+      .map((queriesTemp) => parseInt(queriesTemp, 10));
+  }
 
-    const result = dynamicArray(n, queries);
+  const result = dynamicArray(n, queries);
 
-    ws.write(result.join('\n') + '\n');
+  ws.write(result.join("\n") + "\n");
 
-    ws.end();
+  ws.end();
 }
