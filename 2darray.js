6f7ef96f07ab45590ce223a2186dae080abedbdb
hrupesh <rcashok0@gmail.com>
2020-09-10 17:33:23 +0530

    Auto Commit 😁

--- 2darray.js
+++ 2darray.js
@@ -1,66 +1,58 @@
-"use strict";
+'use strict';
 
-const fs = require("fs");
+const fs = require('fs');
 
 process.stdin.resume();
-process.stdin.setEncoding("utf-8");
+process.stdin.setEncoding('utf-8');
 
-let inputString = "";
+let inputString = '';
 let currentLine = 0;
 
-process.stdin.on("data", (inputStdin) => {
-  inputString += inputStdin;
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
 });
 
-process.stdin.on("end", (_) => {
-  inputString = inputString
-    .replace(/\s*$/, "")
-    .split("\n")
-    .map((str) => str.replace(/\s*$/, ""));
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
 
-  main();
+    main();
 });
 
 function readLine() {
-  return inputString[currentLine++];
+    return inputString[currentLine++];
 }
 
 // Complete the hourglassSum function below.
 function hourglassSum(arr) {
-  var sums = [];
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      let s = 0;
-      s =
-        arr[i][j] +
-        arr[i][j + 1] +
-        arr[i][j + 2] +
-        arr[i + 1][j + 1] +
-        arr[i + 2][j] +
-        arr[i + 2][j + 1] +
-        arr[i + 2][j + 2];
-      console.log(s);
-      sums.push(s);
-    }
-  }
 
-  console.log(sums);
+    var sums = []
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (i < 4 && j < 4) {
+                let s = 0
+                s = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
+                sums.push(s)
+            }
+        }
+    }
+
+    console.log(sums, sums.length)
 }
 
 function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-  let arr = Array(6);
+    let arr = Array(6);
 
-  for (let i = 0; i < 6; i++) {
-    arr[i] = readLine()
-      .split(" ")
-      .map((arrTemp) => parseInt(arrTemp, 10));
-  }
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
 
-  let result = hourglassSum(arr);
+    let result = hourglassSum(arr);
 
-  ws.write(result + "\n");
+    ws.write(result + "\n");
 
-  ws.end();
+    ws.end();
 }
