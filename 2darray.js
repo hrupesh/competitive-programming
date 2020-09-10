a37f0f3a58c28a3ed0b0073308e6a1219d29f726
hrupesh <rcashok0@gmail.com>
2020-09-10 17:06:01 +0530

    Auto Commit 😁

--- 2darray.js
+++ 2darray.js
@@ -1,47 +1,47 @@
-"use strict";
+'use strict';
 
-const fs = require("fs");
+const fs = require('fs');
 
 process.stdin.resume();
-process.stdin.setEncoding("utf-8");
+process.stdin.setEncoding('utf-8');
 
-let inputString = "";
+let inputString = '';
 let currentLine = 0;
 
-process.stdin.on("data", (inputStdin) => {
-  inputString += inputStdin;
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
 });
 
-process.stdin.on("end", (_) => {
-  inputString = inputString
-    .replace(/\s*$/, "")
-    .split("\n")
-    .map((str) => str.replace(/\s*$/, ""));
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
 
-  main();
+    main();
 });
 
 function readLine() {
-  return inputString[currentLine++];
+    return inputString[currentLine++];
 }
 
 // Complete the hourglassSum function below.
-function hourglassSum(arr) {}
+function hourglassSum(arr) {
+
+
+}
 
 function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-  let arr = Array(6);
+    let arr = Array(6);
 
-  for (let i = 0; i < 6; i++) {
-    arr[i] = readLine()
-      .split(" ")
-      .map((arrTemp) => parseInt(arrTemp, 10));
-  }
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
 
-  let result = hourglassSum(arr);
+    let result = hourglassSum(arr);
 
-  ws.write(result + "\n");
+    ws.write(result + "\n");
 
-  ws.end();
+    ws.end();
 }
