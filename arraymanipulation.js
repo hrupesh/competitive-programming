33593b2759ac393cc8e2d84aa7538ddedc9e8640
hrupesh <rcashok0@gmail.com>
2020-09-12 01:36:45 +0530

    Auto Commit 😁

--- arraymanipulation.js
+++ arraymanipulation.js
@@ -1,72 +1,73 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 // Complete the arrayManipulation function below.
 function arrayManipulation(n, queries) {
+  var arr = [];
 
-    var arr = []
+  for (let i = 0; i < n; i++) {
+    let t = 0;
+    arr.push(t);
+  }
 
-    for (let i = 0; i < n; i++) {
-        let t = 0
-        arr.push(t)
+  console.log(arr);
+
+  for (let i = 0; i < queries.length; i++) {
+    for (let j = queries[i][0]; j <= queries[i][1]; j++) {
+      arr[j - 1] += queries[i][2];
+      console.log(i, j);
     }
+  }
 
-    console.log(arr)
+  console.log(arr);
 
-    for (let i = 0; i < queries.length; i++) {
-        for (let j = queries[i][0]; j <= queries[i][1]; j++) {
-            arr[j-1] += queries[i][2]
-            console.log(i, j)
-        }
-    }
-
-    console.log(arr)
-
-    return 0
-
+  return 0;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const nm = readLine().split(' ');
+  const nm = readLine().split(" ");
 
-    const n = parseInt(nm[0], 10);
+  const n = parseInt(nm[0], 10);
 
-    const m = parseInt(nm[1], 10);
+  const m = parseInt(nm[1], 10);
 
-    let queries = Array(m);
+  let queries = Array(m);
 
-    for (let i = 0; i < m; i++) {
-        queries[i] = readLine().split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
-    }
+  for (let i = 0; i < m; i++) {
+    queries[i] = readLine()
+      .split(" ")
+      .map((queriesTemp) => parseInt(queriesTemp, 10));
+  }
 
-    let result = arrayManipulation(n, queries);
+  let result = arrayManipulation(n, queries);
 
-    ws.write(result + "\n");
+  ws.write(result + "\n");
 
-    ws.end();
+  ws.end();
 }
