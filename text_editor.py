f9913c5fa78d5fa182f3b7c065dd04ca954c0be2
hrupesh <rcashok0@gmail.com>
2020-09-12 20:38:36 +0530

    Auto Commit 😁

--- text_editor.py
+++ text_editor.py
@@ -17,18 +17,21 @@ stack = [""]
 
 for i in q:
     if(i[0] == 1):
+        stack.append(S+i[1])
         S += i[1]
-        stack.append(i[1])
+        # print("After Appending: ", S, stack)
     elif(i[0] == 2):
         d = int(i[1])
         S = S[:-d]
-        # stack.append(S[:-d])
+        stack.append(S[:-d])
+        # print("After deleting: ", S, stack)
     elif(i[0] == 3):
         k = int(i[1])
-        if(len(S) > k-1):
-            print(S[k-1])
+        # print(S)
+        print(S[k-1])
     else:
-        S = stack.pop()
+        S = stack.pop(len(stack)-2)
+        # print("Undoing :", S, stack)
 
 
 # print(S, stack)
