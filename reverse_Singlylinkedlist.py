afa657c53805fe1a478855998ff5b577888bf160
hrupesh <rcashok0@gmail.com>
2020-09-15 00:45:40 +0530

    Auto Commit 😁

--- reverse_Singlylinkedlist.py
+++ reverse_Singlylinkedlist.py
@@ -6,10 +6,12 @@ import random
 import re
 import sys
 
+
 class SinglyLinkedListNode:
     def __init__(self, node_data):
         self.data = node_data
         self.next = None
+
 
 class SinglyLinkedList:
     def __init__(self):
@@ -24,8 +26,8 @@ class SinglyLinkedList:
         else:
             self.tail.next = node
 
-
         self.tail = node
+
 
 def print_singly_linked_list(node, sep, fptr):
     while node:
@@ -35,6 +37,7 @@ def print_singly_linked_list(node, sep, fptr):
 
         if node:
             fptr.write(sep)
+
 
 def reverse(head):
     llist = SinglyLinkedList()
@@ -50,9 +53,9 @@ def reverse(head):
         curr = next
 
     llist.head = prev
-    
+
     return llist.head
-        
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
