1bb1470ff58b202e0ae54b5ba0b3f5d39ae3bc36
hrupesh <rcashok0@gmail.com>
2020-09-15 16:50:18 +0530

    Auto Commit 😁

--- merge_singlylinkedlist.py
+++ merge_singlylinkedlist.py
@@ -47,3 +47,30 @@ def mergeLists(head1, head2):
 
 
 if __name__ == '__main__':
+fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist1_count = int(input())
+
+        llist1 = SinglyLinkedList()
+
+        for _ in range(llist1_count):
+            llist1_item = int(input())
+            llist1.insert_node(llist1_item)
+            
+        llist2_count = int(input())
+
+        llist2 = SinglyLinkedList()
+
+        for _ in range(llist2_count):
+            llist2_item = int(input())
+            llist2.insert_node(llist2_item)
+
+        llist3 = mergeLists(llist1.head, llist2.head)
+
+        print_singly_linked_list(llist3, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
