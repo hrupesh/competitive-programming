574b6f0113b1077b48bca673ee08bc21ea5f5b07
hrupesh <rcashok0@gmail.com>
2020-09-15 16:50:42 +0530

    Auto Commit 😁

--- merge_singlylinkedlist.py
+++ merge_singlylinkedlist.py
@@ -47,7 +47,7 @@ def mergeLists(head1, head2):
 
 
 if __name__ == '__main__':
-fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     tests = int(input())
 
@@ -59,7 +59,7 @@ fptr = open(os.environ['OUTPUT_PATH'], 'w')
         for _ in range(llist1_count):
             llist1_item = int(input())
             llist1.insert_node(llist1_item)
-            
+
         llist2_count = int(input())
 
         llist2 = SinglyLinkedList()
