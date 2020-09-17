69baa25b8c63163880a94e41deef510192b897a0
hrupesh <rcashok0@gmail.com>
2020-09-17 19:09:00 +0530

    Auto Commit 😁

--- divisiblesumpairs.py
+++ divisiblesumpairs.py
@@ -7,7 +7,10 @@ import re
 import sys
 
 # Complete the divisibleSumPairs function below.
+
+
 def divisibleSumPairs(n, k, ar):
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
