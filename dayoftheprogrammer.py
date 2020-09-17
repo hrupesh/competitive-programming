84b8f5d8b5537e8b7cd78c186152caf48d689841
hrupesh <rcashok0@gmail.com>
2020-09-18 00:59:53 +0530

    Auto Commit 😁

--- dayoftheprogrammer.py
+++ dayoftheprogrammer.py
@@ -7,19 +7,22 @@ import re
 import sys
 
 # Complete the dayOfProgrammer function below.
-
-
 def dayOfProgrammer(year):
     leap = False
 
-    if year % 4 == 0:
-        leap = True
+    if year > 1918:
+        if year % 4 == 0 and year % 100 != 0:
+            leap = True
+    else:
+        if year % 4 == 0:
+            leap = True
 
     if leap:
         return (f'12.09.{year}')
+    elif year == 1918:
+        return (f'26.09.{year}')
     else:
         return (f'13.09.{year}')
-
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
