3216aaffd89bad068558ba56d0b6b49b3f20fb85
hrupesh <rcashok0@gmail.com>
2020-09-17 19:27:40 +0530

    Auto Commit 😁

--- divisiblesumpairs.py
+++ divisiblesumpairs.py
@@ -6,20 +6,21 @@ import random
 import re
 import sys
 
+
 def divisibleSumPairs(n, k, ar):
-    
+
     pairs = []
     pos_pairs = []
 
     for i in range(n):
         for j in range(n):
             if i != j:
-                pos_pairs.append([ar[i],ar[j]])
+                pos_pairs.append([ar[i], ar[j]])
 
     for i in pos_pairs:
-        print(i[0],i[1])
+        print(i[0], i[1])
 
-    print("Length:",len(pos_pairs))
+    print("Length:", len(pos_pairs))
 
     return 0
 
