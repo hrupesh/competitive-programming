749cba58519dbe40d90627dcbe1f63750d2f1a47
hrupesh <rcashok0@gmail.com>
2020-09-17 20:14:51 +0530

    Auto Commit 😁

--- migratorybirds.py
+++ migratorybirds.py
@@ -6,10 +6,29 @@ import random
 import re
 import sys
 
-# Complete the migratoryBirds function below.
-
-
 def migratoryBirds(arr):
+    b1 = 0
+    b2 = 0
+    b3 = 0
+    b4 = 0
+    b5 = 0
+
+    for i in arr:
+        if i == 1:
+            b1 += 1
+        elif i == 2:
+            b2 += 1
+        elif i == 3:
+            b3 += 1
+        elif i == 4:
+            b4 += 1
+        else:
+            b5 += 1
+
+    s = [b1,b2,b3,b4,b5]
+    print(s)
+
+    return 0
 
 
 if __name__ == '__main__':
