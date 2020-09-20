d34bf3625792b2788b8a7f18bf360a198071fad3
hrupesh <rcashok0@gmail.com>
2020-09-20 16:52:31 +0530

    Auto Commit 😁

--- secondmax.js
+++ secondmax.js
@@ -1,50 +1,49 @@
-'use strict';
+"use strict";
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
     });
-    
-    main();    
+
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
-/**
-*   Return the second largest number in the array.
-*   @param {Number[]} nums - An array of numbers.
-*   @return {Number} The second largest number in the array.
-**/
 function getSecondLargest(nums) {
-    nums = nums.sort(function(a, b){return a - b});
-    console.log(nums)
-    let max = nums[nums.length-1]
-    console.log(max)
-    let i = nums.length-1
-    while (i >= 0){
-        if (nums[i] < max){
-            return nums[i]
-        }
-        i--
+  nums = nums.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(nums);
+  let max = nums[nums.length - 1];
+  console.log(max);
+  let i = nums.length - 1;
+  while (i >= 0) {
+    if (nums[i] < max) {
+      return nums[i];
     }
+    i--;
+  }
 }
-
 
 function main() {
-    const n = +(readLine());
-    const nums = readLine().split(' ').map(Number);
-    
-    console.log(getSecondLargest(nums));
+  const n = +readLine();
+  const nums = readLine().split(" ").map(Number);
+
+  console.log(getSecondLargest(nums));
 }
