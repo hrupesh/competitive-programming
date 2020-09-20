49a86602194b884522f6f65200cf468c5ef7f0c1
hrupesh <rcashok0@gmail.com>
2020-09-20 16:03:00 +0530

    Auto Commit 😁

--- akbarbirbal.py
+++ akbarbirbal.py
@@ -1,9 +1,15 @@
+def cut(sticks):
+    max = sticks[0]
+    for i in sticks:
+        if i > max:
+            max = i
+    print(max)
+
+
 n = int(input())
 for _ in range(n):
     sticks_n = int(input())
     sticks = input().split()
     for i in range(sticks_n):
         sticks[i] = int(sticks[i])
-
-print(sticks)
-print(sticks_n)
+    cut(sticks)
