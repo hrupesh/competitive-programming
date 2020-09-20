5ace5cba9098010fa85b7c914530cc0674e3c7fd
hrupesh <rcashok0@gmail.com>
2020-09-20 15:12:09 +0530

    Auto Commit 😁

--- akbarbirbal.py
+++ akbarbirbal.py
@@ -2,7 +2,8 @@ n = int(input())
 for _ in range(n):
     sticks_n = int(input())
     sticks = input().split()
-
+    for i in range(sticks_n):
+        sticks[i] = int(sticks[i])
 
 print(sticks)
 print(sticks_n)
